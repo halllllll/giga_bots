f635c51b795ad58d467145c2bc0d64a5ede3bdf9
halllllll <h4l@yahoo.ne.jp>
2022-06-10 00:41:01 +0900

    rewrite logic, architecture

--- main.go
+++ main.go
@@ -45,87 +45,99 @@ type Stat struct {
 	MaintenanceStatMsg string
 }
 
-func (stat *Stat) Bot() {
-	if msg, err := stat.ServerStat(); msg != "" && err == nil {
-		var (
-			imgUrl string
-		)
-		if msg == "CAUTION" {
-			imgUrl = cautionImg_url
-			msg = "なにかが起きているようです"
-		} else if msg == "REPAIRED" {
-			imgUrl = repairImg_url
-			msg = "復旧したようです"
-		}
-		dw := &discord.DiscordWebhook{
-			UserName:  fmt.Sprintf("%s_監視ちゃん", stat.Service),
-			AvatarURL: "",
-			Content:   msg,
-			Embeds:    []discord.DiscordEmbed{},
-			TTS:       false,
-		}
-		dw.Embeds = []discord.DiscordEmbed{
-			discord.DiscordEmbed{
-				Title: fmt.Sprintf("%s サーバーステータス", stat.Service),
-				Desc:  "",
-				URL:   stat.Url,
-				Color: 0x550000,
-				Image: discord.DiscordImg{URL: imgUrl},
-			},
-		}
-		if err := discord.SendWebhook(discord_webhookUrl, dw); err != nil {
-			utils.ErrLog.Printf("%s bot sending error: %s", stat.Service, err)
-		}
-	}
+type Loilo struct {
+	Stat
 }
 
-func (stat *Stat) ServerStat() (string, error) {
-	var (
-		ret string
-		err error
-	)
-	doc, err := goquery.NewDocument(stat.Url)
+func (loilo *Loilo) ServerStat() (Stat, bool) {
+	doc, err := goquery.NewDocument(loilo.Url)
 	if err != nil {
-		utils.ErrLog.Printf("error at access status of %s : %v\n", stat.Service, err)
+		utils.ErrLog.Printf("error at access status of %s : %v\n", loilo.Service, err)
 	}
-	switch stat.Service {
-	case "loilo":
-		target := doc.Find(".description-text").Text()
-		if os.Getenv(stat.EnvName) == "OK" && !strings.Contains(target, stat.NormalStatMsg) {
-			// 正常な状態から異常な状態(種類は不明（3種類くらいはある？）)に遷移した
-			utils.InfoLog.Printf("-- !something accidnet occured on SERVICE [ %s ]\n ", stat.Service)
-			if err = os.Setenv(stat.EnvName, "NOT_OK"); err != nil {
-				utils.ErrLog.Println(err)
-			}
-			ret = "CAUTION"
-		} else if os.Getenv(stat.EnvName) != "OK" && strings.Contains(target, stat.NormalStatMsg) {
-			// 異常な状態(種類は不明（3種類くらいはある？）)から正常な状態に戻った
-			utils.InfoLog.Printf("-- !changed status of [ %s ] : SERVICE [ %s ]\n ", os.Getenv(stat.EnvName), stat.Service)
-			if err = os.Setenv(stat.EnvName, "OK"); err != nil {
-				utils.ErrLog.Println(err)
-			}
-			ret = "REPAIRED"
-		} else {
-		}
-	case "l-gate":
-	case "miraiseed":
+	target := doc.Find(".description-text").Text()
+	// 現在が正常な場合かどうかの判定
+	fmt.Println("target... : ", target, strings.Contains(target, loilo.NormalStatMsg))
+	return loilo.Stat, strings.Contains(target, loilo.NormalStatMsg)
+}
 
+type LGate struct {
+	Stat
+}
+
+type MiraiSeed struct {
+	Stat
+}
+
+type Bot interface {
+	ServerStat() (Stat, bool)
+}
+
+func Go(bot Bot) error {
+	// 正常な場合はOK==true
+	stat, ok := bot.ServerStat()
+	pre := os.Getenv(stat.EnvName) // 今回取得する前の状態
+	var (
+		err    error
+		imgUrl string
+		msg    string
+	)
+
+	if pre == "OK" && !ok {
+		// 前回まで正常な状態から今回は異常な状態(種類は不明（3種類くらいはある？）)に遷移した
+		utils.InfoLog.Printf("-- !something accidnet occured on SERVICE [ %s ]\n ", stat.Service)
+		imgUrl = cautionImg_url
+		msg = "なにかが起きているようです"
+		if err = os.Setenv(stat.EnvName, "NOT_OK"); err != nil {
+			utils.ErrLog.Println(err)
+		}
+	} else if pre == "NOT_OK" && ok {
+		// 異常 -> 正常
+		utils.InfoLog.Printf("-- repaired : SERVICE [ %s ]\n ", stat.Service)
+		imgUrl = repairImg_url
+		msg = "復旧したようです"
+		if err = os.Setenv(stat.EnvName, "OK"); err != nil {
+			utils.ErrLog.Println(err)
+		}
+	} else {
+		// 前回と変わらない値のはず
+		utils.StdLog.Printf("[Negative] pre: %s\n", pre)
+		return nil
 	}
-	return ret, err
+
+	// このへんはあとでわけるのをかんがえます
+	dw := &discord.DiscordWebhook{
+		UserName:  fmt.Sprintf("%s_監視ちゃん", stat.Service),
+		AvatarURL: "",
+		Content:   msg,
+		Embeds:    []discord.DiscordEmbed{},
+		TTS:       false,
+	}
+	dw.Embeds = []discord.DiscordEmbed{
+		discord.DiscordEmbed{
+			Title: fmt.Sprintf("%s サーバーステータス", stat.Service),
+			Desc:  "",
+			URL:   stat.Url,
+			Color: 0x550000,
+			Image: discord.DiscordImg{URL: imgUrl},
+		},
+	}
+	if err = discord.SendWebhook(discord_webhookUrl, dw); err != nil {
+		utils.ErrLog.Printf("%s bot sending error: %s", stat.Service, err)
+	}
+	return err
 }
 
 func main() {
-	// loilo
-
-	stat := &Stat{
-		Service:       "loilo",
-		Url:           "https://status.loilonote.app/ja",
-		EnvName:       "LOILO_STATUS",
-		NormalStatMsg: "すべてのサービスが正常に稼働しています",
+	loilo := &Loilo{
+		Stat: Stat{
+			Service:       "loilo",
+			Url:           "https://status.loilonote.app/ja",
+			EnvName:       "LOILO_STATUS",
+			NormalStatMsg: "すべてのサービスが正常に稼働しています",
+		},
 	}
-	stat.Bot()
-
-	// l-gate
+	var bot Bot = loilo
+	Go(bot)
 
 	// l-gate
 
@@ -134,4 +146,9 @@ func main() {
 	// 環境変数を変更してもプロセスを抜けるとなかったことになるので
 	defer syscall.Exec(os.Getenv("SHELL"), []string{os.Getenv("SHELL")}, syscall.Environ())
 
+}
+
+func Say() {
+	fmt.Println("env:")
+	fmt.Println(os.Getenv("LOILO_STATUS"))
 }
